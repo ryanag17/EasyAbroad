cbc59ecef31fc31cdb0b5fe9eaaad2df3a656399
amidovich <amidovich@gmail.com>
2025-05-17 02:06:22 +0200

    adjustments and footer links

--- frontend/script.js
+++ frontend/script.js
@@ -37,6 +37,23 @@ function moveSlide(direction) {
     });
 }
 
+// Register password icon btn
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggles = document.querySelectorAll(".toggle-password");
+
+  toggles.forEach(toggle => {
+    toggle.addEventListener("click", function () {
+      const input = document.querySelector(this.getAttribute("toggle"));
+      const type = input.getAttribute("type") === "password" ? "text" : "password";
+      input.setAttribute("type", type);
+      this.classList.toggle("fa-eye");
+      this.classList.toggle("fa-eye-slash");
+    });
+  });
+});
+
+
 
 // FAQ
 document.addEventListener("DOMContentLoaded", () => {
